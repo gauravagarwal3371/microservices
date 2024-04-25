ea75b16e0153e75d64edbf28d7215ffaa5ffcb43
gauravagarwal3371 <gauravagarwal3371@gmail.com>
2024-04-25 19:38:17 +0400

    Refactor

--- receipt-service/src/main/java/com/codergv/receiptms/service/ReceiptService.java
+++ receipt-service/src/main/java/com/codergv/receiptms/service/ReceiptService.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.Optional;
 
